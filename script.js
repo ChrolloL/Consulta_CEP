46bbd0f6d3a23f090a1c5e2a3eed59698f78a1fa
Lucas Batista <72418534+ChrolloL@users.noreply.github.com>
2022-08-09 11:06:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// Lucas Batista e Felipe Sella
 const cep = document.getElementById('cep')
+const end = document.getElementById('endereco')
+const bairro = document.getElementById('bairro')
+const cidade = document.getElementById('cidade')
+const estado = document.getElementById('estado')
 
 cep.addEventListener('focusout', () => {
-    if (cep.value.length == 6) { console.log(cep.value) }
-    let valor = `https://viacep.com.br/ws/${cep.value}/json/`
-    console.log(valor)
-    
+    if (cep.value.length == 8) { 
+        let valor = `https://viacep.com.br/ws/${cep.value}/json/`
+        $.getJSON(valor, function(result){
+            end.value = result.logradouro
+            bairro.value = result.bairro
+            cidade.value = result.localidade
+            estado.value = result.uf
+        });
+    } else { alert('CEP Inválido. Digite apenas números!')}
 })
